be98b9c56acf6df9f7828b1e544c913be84e7b58
rymo90 <red.01yassin@gmail.com>
2018-07-19 16:14:13 +0200

    added state event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
 class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  handleIncrement = product => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
   render() {
     return (
       <div>
-        <h1>hello world</h1>
-        <button>increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm"
+        >
+          increment
+        </button>
+        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
       </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    const x = <h1>"Zero</h1>;
+    return count === 0 ? x : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Counter from './components/counter'
-ReactDOM.render( < Counter / > , document.getElementById('App'));
+ReactDOM.render( < Counter / > , document.getElementById('root'));
 
 registerServiceWorker();
